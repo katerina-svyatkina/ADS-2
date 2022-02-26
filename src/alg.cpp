179a2ad63008d347aec9fe188c1de8e279f02a17
Екатерина Святкина <99070196+katerina-svyatkina@users.noreply.github.com>
2022-02-26 17:35:24 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,25 +4,48 @@
 
 
 double pown(double value, uint16_t n) {
-
+    double temp = 1;
+    while (n != 0) {
+        n--;
+        temp = temp * value;
+    }
+    return temp;
 }
 
 uint64_t fact(uint16_t n) {
+    if (n == 1 || n==0) {
+        return 1;
+    }
 
+    return n * fact(n - 1);
 }
 
 double calcItem(double x, uint16_t n) {
-
+     return pown(x, n) / fact(n);
 }
 
 double expn(double x, uint16_t count) {
-
+    double temp = 0;
+    while (count != 0) {
+       temp += calcItem(x, count--);
+    }
+    return temp + 1.0;
 }
 
 double sinn(double x, uint16_t count) {
-
+    double temp = 0;
+    while (count != 0) {
+        temp += pown(-1, count-1)*calcItem(x, 2 * count - 1);
+        count--;
+    }
+    return temp;
 }
 
 double cosn(double x, uint16_t count) {
-
+    double temp = 0;
+    while (count != 0) {
+        temp += pown(-1, count - 1) * calcItem(x, 2 * count - 2);
+        count--;
+    }
+    return temp;
 }
